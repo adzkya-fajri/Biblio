89385c8037770e952b91a422e26b4776643aec3f
adzkya-fajri <zelx.arthur@gmail.com>
2025-11-12 10:16:07 +0700

    import package

--- app/src/main/java/ui/components/Profile.kt
+++ app/src/main/java/ui/components/Profile.kt
@@ -1,8 +1,7 @@
 package com.example.biblio.ui.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
