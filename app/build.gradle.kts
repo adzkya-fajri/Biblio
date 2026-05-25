71b5641a1678ff3f6f78f77e55e9b717290e0eb7
Ega-telkom <541241041@student.smktelkom-pwt.sch.id>
2026-05-25 14:32:12 +0700

    add: deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,8 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:5.3.2")
     implementation("com.squareup.moshi:moshi:1.15.2")
     implementation("com.squareup.moshi:moshi-kotlin:1.15.2")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.3.2")
+    implementation("com.squareup.retrofit2:converter-scalars:3.0.0")
     kapt("com.squareup.moshi:moshi-kotlin-codegen:1.15.2")
 
     // ViewModel
